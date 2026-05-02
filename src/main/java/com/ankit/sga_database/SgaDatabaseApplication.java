4698435b8880c8bea43fa3c4b1d1505c11503c89
Ankit Kumar <ankitkr.ak14@gmail.com>
2026-05-02 23:04:24 +0530

    init

---
+++ src/main/java/com/ankit/sga_database/SgaDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.sga_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankit/sga_database/SgaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.sga_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
